1a53b60765aa96e27ea80be4d9d1a0e1eac921bb
ekuinox <depkey@me.com>
2023-01-31 23:03:44 +0900

    2023-01に出たシングルだけを表示してみる

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use anyhow::{bail, Result};
 use rspotify::{
     clients::{BaseClient, OAuthClient},
+    model::{AlbumType, Market},
     scopes, AuthCodePkceSpotify, Config, Credentials, OAuth,
 };
 
@@ -23,11 +24,49 @@ async fn main() -> Result<()> {
         };
         after = Some(next);
     }
-    let len = artists.len();
-    for artist in artists {
-        println!(" - {}", artist.name);
+    // let len = artists.len();
+    // for artist in artists {
+    //     println!(" - {}", artist.name);
+    // }
+    // println!("Length: {}", len);
+    let results = futures::future::join_all(artists.into_iter().map(|artist| {
+        client.artist_albums_manual(
+            artist.id,
+            Some(AlbumType::Single), // ここVec使えないんか～～～い
+            Some(Market::FromToken),
+            Some(50),
+            None,
+        )
+    }))
+    .await;
+
+    if results.iter().any(|r| r.is_err()) {
+        println!("results include any error");
     }
-    println!("Length: {}", len);
+    let albums = results
+        .into_iter()
+        .flatten()
+        .map(|r| r.items)
+        .flatten()
+        .filter(|album| {
+            if let Some(release_data) = &album.release_date {
+                release_data.starts_with("2023-01")
+            } else {
+                false
+            }
+        })
+        .collect::<Vec<_>>();
+
+    for album in albums {
+        let artists = album
+            .artists
+            .into_iter()
+            .map(|artist| artist.name)
+            .collect::<Vec<_>>()
+            .join(", ");
+        let (_, url) = album.external_urls.into_iter().next().unwrap_or_default();
+        println!(" - {} by {} {}", album.name, artists, url);
+    }
 
     Ok(())
 }
